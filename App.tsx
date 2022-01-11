ba1b754582be9a1e25aa79c22df6a2a1d04e09b2
Ewerton Augusto <ewerton.santos@dextra-sw.com>
2022-01-11 11:48:36 -0300

    initial setup of the mobile app

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Services from './src/pages/Services';
+
+const App = () => {
+  return (
+    <View>
+      <Services />
+    </View>
+  );
+};
+
+export default App;
